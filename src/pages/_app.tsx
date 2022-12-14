f7487f854f137d03126c7ed29bb764c51d5f37ad
Bhavesh <bhic2030@gmail.com>
2022-12-14 21:02:28 +0530

    chore: project setup(tailwind, react query, prisma, ts)

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} position={"bottom-right"} ></ReactQueryDevtools>
+    </QueryClientProvider>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import Head from "next/head";
+
+export default function Home() {
+  return <div className="bg-blue-200">
+    Make it
+  </div>;
+}
